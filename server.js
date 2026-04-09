23b287a503b8893576fe3452ab24cbf41b8f38a5
Henry-3551 <edmundchukwuemerie22@gmail.com>
2026-04-09 17:19:51 +0100

    Update server.js

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ const escapeHtml = (value) => {
 
 const contactLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 10,
+  max: 3,
   standardHeaders: true,
   legacyHeaders: false,
   message: { error: "Too many requests. Please try again later." }
